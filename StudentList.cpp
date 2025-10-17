c2a45a38a0da241ce706eebbda591547e8fc8bb4
U-BSD\453144 <453144@SHS-C5W2NW3.beaverton.k12.or.us>
2025-10-17 12:23:16 -0700

    Final StudentList C++

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,9 +1,17 @@
+/*
+  Ella MacPherson
+  C++ StudentList
+  10/17/25
+  Create a list of students that can have additions, deletions and be printer oud
+*/
+
 #include <iostream>
 #include <cstring>
-#include <vector> //kind of like arrays but can change in size
+#include <vector>
 
 using namespace std;
 
+//create Student struct to hold all attributes
 struct Student{
   char first[10];
   char last[10];
@@ -18,56 +26,60 @@ void Delete(vector<Student*>& vS);
 
 
 int main(){
-  //var to track what they want to do
   bool go = true;
 
-  //create vector of students
+  //create vector of pointers to student structs
   vector<Student*> vS;
-  
+
+  //loop that goes forever until main returns
   while(go == true){
+    //cstring to hold input
     char input[10];
     cout<<"Would you like to: ADD, DELETE, PRINT, or QUIT" << endl; 
     cin.get(input, 9);
-    cin.ignore();
+    cin.ignore(); //clear buffer
     
-    if(strcmp(input, "ADD") == 0){
+    if(strcmp(input, "ADD") == 0 || strcmp(input, "add") == 0){
       Add(vS);
     }
-    else if(strcmp(input, "DELETE") == 0){
+    else if(strcmp(input, "DELETE") == 0 || strcmp(input, "delete") == 0){
       Delete(vS);
     }
-    else if(strcmp(input, "PRINT") == 0){
+    else if(strcmp(input, "PRINT") == 0 || strcmp(input, "print") == 0){
       Print(vS);
     }
-    else if(strcmp(input, "QUIT") == 0){
+    else if(strcmp(input, "QUIT") == 0 || strcmp(input, "quit") == 0){
       return 0;
     }
     else{
-      cout<<"Please enter a valid command";
-      break;
+      cout<<"Please enter a valid command" <<endl;
+      input[0] = '\0'; //clear input
     }
   }
 }
 
+//Print function
 void Print(vector<Student*>& vS){
-  cout<<"Print"<<endl;
+  //loop through vector, access each element in the struct corresponding to the index
   for(int i =0; i < vS.size(); i++){
     cout<<i+1<<". ";
-    cout<<vS[i]->first<<" "; //thake pointer and point to memory and access first in that
+    cout<<vS[i]->first<<" ";
     cout<<vS[i]->last<<", ";
     cout<<vS[i]->id<<", ";
     cout<<vS[i]->gpa<<endl;
   }
 }
 
- 
-void Add(vector<Student*>& vS){ //BREAKS when: char is entered into ID or GPA, sometimes weird char at end look at
+//Add function
+void Add(vector<Student*>& vS){
+  //create new student pointer
+  //create cstrings to hold inputs
   Student*s = new Student;
-  char FIRST[10];
+  char FIRST[20];
   char LAST[80];
   
   cout<<"Enter the first name: "<<endl;
-  cin.get(FIRST, 9);
+  cin.get(FIRST, 20);
   cin.ignore();
   strcpy(s->first,FIRST);
  
@@ -83,32 +95,36 @@ void Add(vector<Student*>& vS){ //BREAKS when: char is entered into ID or GPA, s
   cout<<"Enter the GPA: "<<endl;
   cin>>s->gpa;
   cin.ignore();
-  
+
+  //Add the student pointer with all the new defined elements into the vector
   vS.push_back(s);
 }
 
- 
+
+//Delete function
 void Delete(vector<Student*>& vS){
   cout<<"Enter the ID number of the student you'd like to delete: "<<endl;
+  //var to hold id to be deleted and to hold if that number exists
   int idDelete;
   bool exists = false;
 
   cin>>idDelete;
   cin.ignore();
-  
+
+  //use iterator to go through vector
   for(auto it = vS.begin(); it != vS.end(); it++){
-    //if the id matches the delete
+    //if the id matches then delete
     if((*it)->id == idDelete){
       exists = true;
       delete (*it);
       vS.erase(it);
       return;
     }
-    if(exists == false){
-      cout<<"ID not found, try again."<<endl;
-    }
   }
-  
+  //if id doesnt exist inform user
+  if(exists == false){
+    cout<<"ID not found, try again."<<endl;
+  }
 }
 
  
